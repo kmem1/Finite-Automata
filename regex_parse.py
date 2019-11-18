7cf588f19e4e5dc739ce3f7db5eeaf9eb156d295
Alex <52524449+kmem1@users.noreply.github.com>
2019-11-18 15:57:16 +0300

    Update regex_parse.py

--- regex_parse.py
+++ regex_parse.py
@@ -1,27 +1,6 @@
 import NFA
 
 
-class Scanner:
-    def init(self, data_):
-        self.data = _preprocess(data_)
-        self.next = 0
-
-    def peek(self):
-        return self.data[self.next] if self.next < len(self.data) else 0
-
-    def pop(self):
-        cur = self.peek()
-        if( self.next < len(self.data) ):
-            self.next += 1
-        return cur
-
-    def get_pos(self):
-        return self.next
-
-
-my_scanner = Scanner()
-
-
 class ParseNode:
     def __init__(self, type_, data_, left_, right_):
         self.type = type_
@@ -58,5 +37,59 @@ def _preprocess(data):
 
     return out
 
+def _chr():
+    data = my_scanner.peek()
+    
+    if data.isalnum() or data == 0:
+        return ParseNode('CHR', my_scanner.pop(),0,0)
+    
+    raise Exception
 
+def _atom():
+    atom_node = ParseNode()
+    
+    if my_scanner.peek() == '(':
+        my_scanner.pop()
+        atom_node = _expr()
+        
+        if my_scanner.pop() != ')':
+            raise Exception("Need ')' ")
+            
+    else:
+        atom_node = _chr()
+    
+    return atom_node
+
+def _rep():
+    atom_node = _atom()
+    
+    if my_scanner.peek() == '*':
+        my_scanner.pop()
+        return ParseNode('STAR', 0, atom_node, 0)
+    elif my_scanner.peek() == '?':    
+        my_scanner.pop()
+        return ParseNode('QUESTION', 0, atom_node, 0)
+    else:
+        return atom_node
+
+def _concat():
+    left = _rep()
+    
+    if my_scanner.peek() == '.':
+        my_scanner.pop()
+        right = _concat()
+        return ParseNode('CONCAT', 0, left, right)
+    else:
+        return left
+        
+def _expr():
+    left = _concat()
+    
+    if my_scanner.peek() == '|':
+        my_scanner.pop()
+        right = _expr()
+        return ParseNode('ALTER', 0, left, right)
+    else:
+        return left
+    
 
